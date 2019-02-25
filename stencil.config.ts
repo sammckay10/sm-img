832e77c90e23f3b1e36199194f3ff7a74fe7cd61
Sam McKay <sammckay@Sams-MacBook-Pro.local>
2019-02-25 15:52:30 +0000

    Config stuff

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,12 +2,5 @@ import { Config } from '@stencil/core'
 
 export const config: Config = {
   namespace: 'sm-img',
-  outputTargets: [
-    { type: 'dist' },
-    { type: 'docs' },
-    {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
+  outputTargets: [{ type: 'dist' }, { type: 'docs' }]
 }
